5132983084c67736a8dcc0f57cc57aa06fb17b4d
User <user@example.com>
2026-02-16 23:42:33 +0700

    Add PostgreSQL port 8080 gunicorn req.txt ignore

--- config.py
+++ config.py
@@ -8,7 +8,21 @@ class Config:
     
     # For Cloud Run, use Cloud SQL or external database
     # Format: postgresql://user:password@/dbname?host=/cloudsql/project:region:instance
-    DATABASE_URL = os.environ.get('DATABASE_URL')
+    
+    #DATABASE_URL = os.environ.get('DATABASE_URL')
+
+    # Get these from Environment Variables (Best Practice)
+    DB_USER = os.environ.get("DB_USER") or 'postgres'
+    DB_PASS = os.environ.get("DB_PASS") or 'nott5036673'
+    DB_NAME = os.environ.get("DB_NAME") or 'it_resource_manager'
+    # This is the "Connection Name" from the Cloud SQL Overview page
+    INSTANCE_CONNECTION_NAME = os.environ.get("INSTANCE_CONNECTION_NAME") or 'project-9b468cf0-2a93-45dc-860:europe-west1:free-trial-first-project'
+
+    # The format for Unix Sockets with Psycopg2:
+    DATABASE_URL = (
+        f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@/{DB_NAME}?"
+        f"host=/cloudsql/{INSTANCE_CONNECTION_NAME}"
+    )
     
     if DATABASE_URL:
         # Handle Cloud SQL Unix socket connection
